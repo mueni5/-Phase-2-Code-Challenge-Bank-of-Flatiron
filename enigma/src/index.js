546f14337cc311dbf3f465a7c74b958f0179d36a
+ <+>
2023-04-25 00:06:25 +0300

    Complete

--- enigma/src/index.js
+++ enigma/src/index.js
@@ -9,3 +9,21 @@ root.render(
     <h1>The Royal Bank of Flatiron</h1>
   </React.StrictMode>
 );
+var settings = {
+    "url": "localhost:3000/transactions/1",
+    "method": "GET",
+    "timeout": 0,
+    "headers": {
+      "Content-Type": "application/json"
+    },
+    "data": JSON.stringify({
+      "date": "string",
+      "description": "string",
+      "category": "string",
+      "amount": "number"
+    }),
+  };
+  
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+  });
