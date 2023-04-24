258464e420c14581c75caad38bc162538318daf3
+ <+>
2023-04-24 16:08:20 +0300

    Started

---
+++ enigma/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <h1>The Royal Bank of Flatiron</h1>
+  </React.StrictMode>
+);
+
