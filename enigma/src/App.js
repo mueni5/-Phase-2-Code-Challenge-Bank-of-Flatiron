f1c547f4d44b2561134cd76b638d4d9af4ae9de4
+ <+>
2023-04-24 22:34:10 +0300

    Completed assignment

--- enigma/src/App.js
+++ enigma/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import TransTable from './TransTable';
+import index from './index';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <index/>
+      <TransTable/>
+      <AddTransaction/>
+    </>
   );
-}
+  }
+
 
 export default App;
